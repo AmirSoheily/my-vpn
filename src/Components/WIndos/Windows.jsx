4f9d9828004e8c610bb1be1677f3812b26adeb71
AmirSoheily <109553383+AmirSoheily@users.noreply.github.com>
2022-11-09 22:05:05 -0800

    herfWindosdelet

--- src/Components/WIndos/Windows.jsx
+++ src/Components/WIndos/Windows.jsx
@@ -3,12 +3,7 @@ import { Icon } from "@iconify/react";
 
 function Windows() {
   return (
-    <a
-      className="font_family"
-      style={{ textDecoration: "none" }}
-      href="https://drive.google.com/file/d/1tbDJ8XjVXdqGiMk5nyKVaSRIjZcYFI5S/view?usp=sharing"
-      download
-    >
+    <a className="font_family" style={{ textDecoration: "none" }} download>
       <div className="windosbtn">
         <Icon fontSize={200} icon="logos:microsoft-windows" />
         <div> دانلود ورژن ویندوزs</div>
