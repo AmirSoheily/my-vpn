156677c83195c4cdd69eaa86f3675b056ba6a36d
AmirSoheily <109553383+AmirSoheily@users.noreply.github.com>
2022-11-09 19:39:51 -0800

    ss

--- src/Components/WIndos/Windows.jsx
+++ src/Components/WIndos/Windows.jsx
@@ -11,7 +11,7 @@ function Windows() {
     >
       <div className="windosbtn">
         <Icon fontSize={200} icon="logos:microsoft-windows" />
-        <div> دانلود ورژن ویندوز</div>
+        <div> دانلود ورژن ویندوزs</div>
       </div>
     </a>
   );
