839ce3f6dd9b5acf5e2f738721e06a29819ea883
AmirSoheily <109553383+AmirSoheily@users.noreply.github.com>
2022-11-19 13:01:43 -0800

    sss

--- src/Components/WIndos/Windows.jsx
+++ src/Components/WIndos/Windows.jsx
@@ -3,7 +3,12 @@ import { Icon } from "@iconify/react";
 
 function Windows() {
   return (
-    <a className="font_family" style={{ textDecoration: "none" }} download>
+    <a
+      href="https://drive.google.com/file/d/1tbDJ8XjVXdqGiMk5nyKVaSRIjZcYFI5S/view?usp=sharing"
+      className="font_family"
+      style={{ textDecoration: "none" }}
+      download
+    >
       <div className="windosbtn">
         <Icon fontSize={200} icon="logos:microsoft-windows" />
         <h1>کص مادرت نمیا</h1>
