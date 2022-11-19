bcc082a14b7bbe4f3a675ce21ac73315e6b48281
AmirSoheily <109553383+AmirSoheily@users.noreply.github.com>
2022-11-19 12:55:05 -0800

    سس

--- src/Components/WIndos/Windows.jsx
+++ src/Components/WIndos/Windows.jsx
@@ -6,6 +6,7 @@ function Windows() {
     <a className="font_family" style={{ textDecoration: "none" }} download>
       <div className="windosbtn">
         <Icon fontSize={200} icon="logos:microsoft-windows" />
+        <h1>کص مادرت نمیا</h1>
         <div>دانلود ورژن کامپیوتر</div>
       </div>
     </a>
